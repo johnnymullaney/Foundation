2fdb7192c32f22baa7a1dc32af30bc4709ae39df
Jonathan Mullaney <jonathan.mullaney@madetoengage.com>
2021-12-02 11:11:18 +0000

    Custom DataCaptureForm Container block with razor template

---
+++ src/Foundation/Features/Forms/DataCaptureContainerBlock.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Forms.Core;
+using EPiServer.Forms.Implementation.Elements;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Foundation.Features.Forms
+{
+    [ContentType(
+        DisplayName = "Data Capture Form Container",
+        GUID = "16660C0A-5129-4E6C-85C0-E16161842F2D",
+        GroupName = "DataCaptureContainerElements")]
+    [ServiceConfiguration(typeof(IFormContainerBlock))]
+    public class DataCaptureContainerBlock : FormContainerBlock
+    {
+        [Display(Name = "Main Image", GroupName = SystemTabNames.Content)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference MainImage { get; set; }
+    }
+}
---
+++ src/Foundation/Features/Forms/DataCaptureContainerBlockController.cs
@@ -0,0 +1,29 @@
+﻿using EPiServer.Forms.Controllers;
+using EPiServer.Forms.Implementation.Elements;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
+using System.Web.Mvc;
+
+namespace Foundation.Features.Forms
+{
+
+    [TemplateDescriptor(
+        AvailableWithoutTag = true,
+        Default = true,
+        ModelType = typeof(DataCaptureContainerBlock),
+        TemplateTypeCategory = TemplateTypeCategories.MvcPartialController)]
+    public class DataCaptureContainerBlockController : FormContainerBlockController
+    {
+        public override ActionResult Index(FormContainerBlock currentBlock)
+        {
+            var partialResult = base.Index(currentBlock);
+
+            if (currentBlock is DataCaptureContainerBlock dataCaptureContainerBlock)
+            {
+                return PartialView("~/Features/Shared/Views/ElementBlocks/DataCaptureContainerBlock.cshtml", dataCaptureContainerBlock);
+            }
+
+            return partialResult;
+        }
+    }
+}
